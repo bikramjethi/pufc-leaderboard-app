720c0fa473b97fbb4cdfbd43bc660cd3d1c9bdfc
bikramjethi <bikramjethi@gmail.com>
2026-01-03 10:45:32 +0530

    add defaullt shell data

--- src/utils/get-data.js
+++ src/utils/get-data.js
@@ -1,10 +1,12 @@
 // Import all season data
 import data2024 from "../data/leaderboard-data/2024.json";
 import data2025 from "../data/leaderboard-data/2025.json";
+import data2026 from "../data/leaderboard-data/2026.json";
 
 const leaderboardStaticData = {
     2024: data2024,
     2025: data2025,
+    2026: data2026,
 };
 
 export const leaderboardData = leaderboardStaticData;
