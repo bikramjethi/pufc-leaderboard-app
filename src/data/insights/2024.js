39ce08c6fe6486c3595194dd2543197f5746d4d6
bikramjethi <bikramjethi@gmail.com>
2026-01-04 14:41:17 +0530

    updated trivia

--- src/data/insights/2024.js
+++ src/data/insights/2024.js
@@ -1,5 +1,8 @@
 export const trivia2024 = [
+  "Sourabh had the highest win % of the season at 55% win rate",
   "Defenders scored 22.6% of total goals",
+  "David lost a whopping 71% of games and won only 14% of games",
   "Jethi Grabbed 23.4% of total club hat tricks with a hat-trick every 4.5 games with Rishav a close 2nd at 17%",
   "Defender Ashish outscored most forwards with 67 goals and 6 hat-tricks",
+  "Five players averaged 1 OR more goal per match in 2024 - Jethi: 1.42 GPM, Aniket: 1.4 GPM, David: 1.05 GPM, Lalit and Soumya were close with 1.0 GPM"
 ];
--- src/data/insights/2025.js
+++ src/data/insights/2025.js
@@ -1,13 +1,14 @@
 export const trivia2025 = [
   "Prateek: 47 matches, 0 goals (true defender?)",
+  "4 players averaged 1.0 or more goals per match in 2025 - Jethi: 2.31 GPM(111 goals), Mathew: 2.0 GPM(62 goals), Vinoth: 1.62 GPM(42 goals),Soumyadeep: 1.07 GPM(30 goals).",
   "Ashish had a whopping 96% attendance in midweek games and overall 92% attendance in games",
   "Mathew perfect no-draws (18W-13L) in 31 games",
   "Jethi averaged 2.31 goals per match—hat-trick every 2.8 games and top scored with 111 goals",
   "Pandey +19 games vs 2024; Sourabh +28 (breakouts).",
   "David Back-to-back 'Unluckiest' with 71% loss in 2024 and 65% losses in 2025",
-  "Vinit lost the most games with 42 overall losses",
+  "Vinit lost the most games in back to back seasons with 38 losses in 2024 and 42 losses in 2025",
   "Jethi had highest win % of 67% as well as lowest loss % of 27%",
   "Ashish crossed the 50 game win mark with 52 overall wins",
   "Subarna had the most number of draws with 8 overall draws",
-  "Jethi scored 25.8% of all 2025 hat-tricks"
+  "Jethi scored 25.8% of all 2025 hat-tricks",
 ];
