1c5514bebd966dcf37d0c5321c57d407407985d1
bikramjethi <bikramjethi@gmail.com>
2026-01-04 14:19:00 +0530

    added trivia

--- src/data/insights/2024.js
+++ src/data/insights/2024.js
@@ -1,4 +1,5 @@
 export const trivia2024 = [
+  "Defenders scored 22.6% of total goals",
+  "Jethi Grabbed 23.4% of total club hat tricks with a hat-trick every 4.5 games with Rishav a close 2nd at 17%",
   "Defender Ashish outscored most forwards with 67 goals and 6 hat-tricks",
 ];
-
