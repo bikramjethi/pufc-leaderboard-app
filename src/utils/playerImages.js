3304bbd5b356ea8502263a41b9f5ca872a1575cc
bikramjethi <bikramjethi@gmail.com>
2026-01-07 18:57:53 +0530

    pandey/prateek/joe

--- src/utils/playerImages.js
+++ src/utils/playerImages.js
@@ -6,6 +6,9 @@ import jethiImage from '../assets/player-profiles/jethi.jpg';
 import vinitImage from '../assets/player-profiles/vinit.jpg';
 import rahulDhekImage from '../assets/player-profiles/rahuldhek.jpg';
 import sudeepImage from '../assets/player-profiles/sudeep.jpg';
+import prateekImage from "../assets/player-profiles/prateek.jpeg";
+import pandeyImage from "../assets/player-profiles/pandey.jpeg";
+import joeImage from "../assets/player-profiles/joe.jpeg";
 
 // Map player names (lowercase) to their profile images
 const playerImageMap = {
@@ -13,6 +16,9 @@ const playerImageMap = {
   'vinit': vinitImage,
   "rahul dhek": rahulDhekImage,
   "sudeep": sudeepImage,
+  "pandey": pandeyImage,
+  "joe": joeImage,
+  "prateek": prateekImage
   // Add more player images here as they become available
   // 'ashish': ashishImage,
   // 'vinay': vinayImage,
