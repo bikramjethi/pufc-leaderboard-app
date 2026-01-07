e5f3d363a031be3623f1f78ffced160aba539a5a
Bikram Jethi <bikramjethi@gmail.com>
2026-01-07 19:34:23 +0530

    add vikalp image

--- src/utils/playerImages.js
+++ src/utils/playerImages.js
@@ -9,6 +9,7 @@ import sudeepImage from '../assets/player-profiles/sudeep.jpg';
 import prateekImage from "../assets/player-profiles/prateek.jpeg";
 import pandeyImage from "../assets/player-profiles/pandey.jpeg";
 import joeImage from "../assets/player-profiles/joe.jpeg";
+import vikalpImage from "../assets/player-profiles/vikalp.jpeg";
 
 // Map player names (lowercase) to their profile images
 const playerImageMap = {
@@ -18,7 +19,8 @@ const playerImageMap = {
   "sudeep": sudeepImage,
   "pandey": pandeyImage,
   "joe": joeImage,
-  "prateek": prateekImage
+  "prateek": prateekImage,
+  "vikalp": vikalpImage
   // Add more player images here as they become available
   // 'ashish': ashishImage,
   // 'vinay': vinayImage,
@@ -31,7 +33,7 @@ const playerImageMap = {
  */
 export const getPlayerImage = (playerName) => {
   if (!playerName) return defaultImage;
-  
+
   const normalizedName = playerName.toLowerCase().trim();
   return playerImageMap[normalizedName] || defaultImage;
 };
