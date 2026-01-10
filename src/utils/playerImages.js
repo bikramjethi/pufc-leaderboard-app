21c4dcc4dc8892fe58ae4cff9b061a43f417ed58
Bikram Jethi <bikramjethi@gmail.com>
2026-01-10 10:24:42 +0530

    change mapping logic

--- src/utils/playerImages.js
+++ src/utils/playerImages.js
@@ -2,29 +2,31 @@
 // Add player-specific images here as they become available
 
 import defaultImage from '../assets/player-profiles/default.jpeg';
-import jethiImage from '../assets/player-profiles/jethi.jpg';
-import vinitImage from '../assets/player-profiles/vinit.jpg';
+import jethi from '../assets/player-profiles/jethi.jpg';
+import vinit from '../assets/player-profiles/vinit.jpg';
 import rahulDhekImage from '../assets/player-profiles/rahuldhek.jpg';
-import sudeepImage from '../assets/player-profiles/sudeep.jpg';
-import prateekImage from "../assets/player-profiles/prateek.jpeg";
-import pandeyImage from "../assets/player-profiles/pandey.jpeg";
-import joeImage from "../assets/player-profiles/joe.jpeg";
-import rishavImage from "../assets/player-profiles/rishav.jpeg";
-import vikalpImage from "../assets/player-profiles/vikalp.jpeg";
-import animeshImage from "../assets/player-profiles/animesh.jpg";
+import sudeep from '../assets/player-profiles/sudeep.jpg';
+import prateek from "../assets/player-profiles/prateek.jpeg";
+import pandey from "../assets/player-profiles/pandey.jpeg";
+import joe from "../assets/player-profiles/joe.jpeg";
+import rishav from "../assets/player-profiles/rishav.jpeg";
+import vikalp from "../assets/player-profiles/vikalp.jpeg";
+import animesh from "../assets/player-profiles/animesh.jpg";
+import ashish from "../assets/player-profiles/ashish.jpeg";
 
 // Map player names (lowercase) to their profile images
 const playerImageMap = {
-  'jethi': jethiImage,
-  'vinit': vinitImage,
+  jethi,
+  vinit,
   "rahul dhek": rahulDhekImage,
-  "sudeep": sudeepImage,
-  "pandey": pandeyImage,
-  "joe": joeImage,
-  "prateek": prateekImage,
-  "rishav": rishavImage,
-  "vikalp": vikalpImage,
-  "animesh": animeshImage,
+  sudeep,
+  pandey,
+  joe,
+  prateek,
+  rishav,
+  vikalp,
+  animesh,
+  ashish
   // Add more player images here as they become available
   // 'ashish': ashishImage,
   // 'vinay': vinayImage,
