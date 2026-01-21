26d0313d8a31e720c0ce175938f94b5100d44457
bikramjethi <bikramjethi@gmail.com>
2026-01-21 20:13:27 +0530

    uma image update

--- src/utils/playerImages.js
+++ src/utils/playerImages.js
@@ -25,7 +25,7 @@ import vivek from "../assets/player-profiles/vivek.jpeg";
 import tushar from "../assets/player-profiles/tushar.jpeg";
 import deepak from "../assets/player-profiles/deepak.jpeg";
 import rohitHyd from "../assets/player-profiles/rohit-hyd.jpeg";
-import umaImage from "../assets/player-profiles/uma2.jpg";
+import umaImage from "../assets/player-profiles/uma.jpg";
 
 
 // Map player names (lowercase) to their profile images
